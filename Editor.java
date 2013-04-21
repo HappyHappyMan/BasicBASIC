1a6254c24f4281af46978eb3a6c2eb5311a25ea1
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2013-04-21 01:19:47 -0400

    Updates to main loop

--- Editor.java
+++ Editor.java
@@ -8,24 +8,35 @@ public class Editor {
     public static void main(String[] args) {
     LinkedList lines = new LinkedList();
     Scanner in = new Scanner(System.in);
+    Dictionary vars = new Dictionary();
 
     System.out.println("Welcome to the testing release of this BASIC interpreter!");
 
     while(true) {
       System.out.print("> ");
       String input = in.nextLine();
+      String keyword = input.split(" ")[0].toUpperCase();
       
-      if (input.equals("RESEQUENCE")) {
+      if (keyword.equals("RESEQUENCE")) {
         lines.resequence();
-      } else if (input.equals("LIST")) {
+      } else if (keyword.equals("LIST")) {
         System.out.println(lines);
+      } else if (keyword.equals("ACCEPT")) {
+          String var = input.split(" ")[1];
+          System.out.print(var + "= ");
+          String num = in.nextLine();
+          vars.put(var, Double.parseDouble(num));
+      } else if (keyword.equals("PRINT")) {
+          String var = input.split(" ")[1];
+          System.out.println(var + " = " + vars.get(var));
       } else {
         //System.out.println("default case");
         String[] tokens = input.split(" ", 2);
         try {
           Integer.parseInt(tokens[0]);
-          lines.insert(input); //to insert a line
+          lines.insert(input);                  //to insert a line
         } catch (NumberFormatException e) {
+          //TODO: need to implement direct interfacing. For now, an error msg.
           System.out.println("Invalid input.");
         }
       }
